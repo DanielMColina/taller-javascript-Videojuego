41712606e43c585c1c09eb696e3b1fb00acc665d
danielmcolina <danielmcolina12@gmail.com>
2023-08-22 18:17:30 -0400

    se agrego la posicion del jugador

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
+const btnUp= document.querySelector("#up")
+const btnLeft = document.querySelector("#left")
+const btnRight = document.querySelector("#right")
+const btnDown = document.querySelector("#down")
 
 window.addEventListener("load",setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
@@ -7,6 +11,13 @@ window.addEventListener("resize", setCanvasSize);
 let canvasSize;
 let elementsSize;
 let correccion;
+
+
+const playerPosition = {
+    x: undefined,
+    y: undefined
+
+}
 
 function setCanvasSize(){
     if(window.innerHeight > window.innerWidth){
@@ -31,7 +42,7 @@ function startGame(){
     game.font= elementsSize + "px verdana";
     game.textAlign= "end";
    
-    const map = maps[1];
+    const map = maps[0];
     const mapRows = map.trim().split("\n");
     const mapRowCols = mapRows.map(rows => rows.trim().split(""));
     console.log(mapRowCols)
@@ -42,9 +53,11 @@ function startGame(){
         const posX = (elementsSize * (colI+1)) + correccion
         const posY = (elementsSize * (rowI+1)) - correccion
 
-
+        if(col == "O"){
+            playerPosition.x = posX;
+            playerPosition.y = posY;
+        }
         game.fillText(emoji,posX,posY )
-        console.log(elementsSize)
        })
     });
     
@@ -53,4 +66,44 @@ function startGame(){
     //     game.fillText(emojis[mapRowCols[row -1][col -1]], (elementsSize *col) + correccion, (elementsSize *row) - correccion)
     //     }
     // }
+    movePlayer();
 }
+
+
+function movePlayer(){
+    game.fillText(emojis["PLAYER"],playerPosition.x,playerPosition.y)
+}
+
+btnUp.addEventListener("click",moveUp);
+btnLeft.addEventListener("click",moveLeft);
+btnRight.addEventListener("click",moveRight);
+btnDown.addEventListener("click",moveDown);
+window.addEventListener("keydown",moveBykeys);
+
+function moveBykeys(event){
+    if (event. key == 'ArrowUp') moveUp() ;
+    else if (event.key == 'ArrowLeft' ) moveLeft ();
+    else if (event.key == 'ArrowRight' ) moveLeft ();
+    else if (event.key == 'ArrowDown' ) moveLeft ();
+}
+function clearMap(){
+    game.clearReact(0,0, canvasSize, canvasSize)
+}
+
+function moveUp() {
+    clearMap()
+    playerPosition.y -= elementsSize 
+    movePlayer();
+  };
+  
+  function moveLeft() {
+    playerPosition.Y - elementsSize ;
+  };
+  
+  function moveRight() {
+    console.log("Me movere hacia dere");
+  };
+  
+  function moveDown() {
+    console.log("Me movere hacia aba");
+  };
