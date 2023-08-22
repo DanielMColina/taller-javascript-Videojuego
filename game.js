6c1bec242c0be0f8a52d26b041241d717f6eaf00
danielmcolina <danielmcolina12@gmail.com>
2023-08-22 11:18:15 -0400

    realice el renderizado de los mapa del juego

---
+++ game.js
@@ -0,0 +1,56 @@
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
+
+window.addEventListener("load",setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
+
+let canvasSize;
+let elementsSize;
+let correccion;
+
+function setCanvasSize(){
+    if(window.innerHeight > window.innerWidth){
+        canvasSize = window.innerWidth * 0.75
+    }else {
+        canvasSize = window.innerHeight * 0.75
+    };
+
+    // window.innerHeight > window.innerWidth ? canvasSize = window.innerWidth * 0.75: canvasSize =window.innerHeight * 0.75
+
+    canvas.setAttribute("width", canvasSize);
+    canvas.setAttribute("height",canvasSize);
+
+    correccion = canvasSize / 50;
+    elementsSize = canvasSize / 10;
+
+    startGame();
+}
+
+function startGame(){
+
+    game.font= elementsSize + "px verdana";
+    game.textAlign= "end";
+   
+    const map = maps[1];
+    const mapRows = map.trim().split("\n");
+    const mapRowCols = mapRows.map(rows => rows.trim().split(""));
+    console.log(mapRowCols)
+
+    mapRowCols.forEach((row, rowI) => {
+       row.forEach((col, colI)=>{
+        const emoji = emojis[col]
+        const posX = (elementsSize * (colI+1)) + correccion
+        const posY = (elementsSize * (rowI+1)) - correccion
+
+
+        game.fillText(emoji,posX,posY )
+        console.log(elementsSize)
+       })
+    });
+    
+    // for (let row = 1; row <= 10; row++ ){
+    //     for(let col = 1; col <= 10; col++){
+    //     game.fillText(emojis[mapRowCols[row -1][col -1]], (elementsSize *col) + correccion, (elementsSize *row) - correccion)
+    //     }
+    // }
+}
